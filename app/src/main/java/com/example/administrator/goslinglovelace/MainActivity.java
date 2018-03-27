803e362a36ff2956a12f95dd5e010d1efeeca2f3
Chen1738 <1819257305@qq.com>
2018-03-27 21:05:02 +0800

    improves

--- app/src/main/java/com/example/administrator/goslinglovelace/MainActivity.java
+++ app/src/main/java/com/example/administrator/goslinglovelace/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.administrator.goslinglovelace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.ada_text);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        //TextView textView2 = (TextView) findViewById(R.id.james_text);
+        //textView2.setMovementMethod(new ScrollingMovementMethod());
     }
 }
